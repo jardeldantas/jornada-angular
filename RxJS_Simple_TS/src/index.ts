c82fb7465335fba9c3ee67b3d89948540d73edc6
jardeldantas <jardel.dantas@outlook.com>
2024-02-24 23:10:00 -0300

    RxJS e Type Scrypt

---
+++ RxJS_Simple_TS/src/index.ts
@@ -0,0 +1,62 @@
+import { Observable, delay, fromEvent, takeWhile, retryWhen, scan, switchMap, retry } from 'rxjs'; 
+
+interface IMovie {
+  title: string;
+}
+
+let button = document.getElementById('button');
+let output = document.getElementById('output');
+
+let click = fromEvent(button, 'click')
+
+function load(url: string): Observable<any>{
+  return new Observable(subscriber => {
+    let xhr = new XMLHttpRequest();
+    output.innerHTML = '';
+  
+    xhr.addEventListener('load', () => {
+      if(xhr.status == 200){
+        let  data = JSON.parse(xhr.responseText);
+        subscriber.next(data); 
+        subscriber.complete();
+      }else {
+        subscriber.error(xhr.statusText);
+      }
+
+    })
+  
+    xhr.open('GET', url);
+    xhr.send();
+  }).pipe(
+    //retryWhen(retryStrategy({ attempt: 3, timeDelay: 1000})) //depreciado
+    retry({count: 3, delay: 1000})
+  )
+}
+
+function retryStrategy({ attempt = 3, timeDelay = 1000}) {
+  return (erros: Observable<any>) => {
+    return erros.pipe(
+      scan((acc, value) => {
+        return acc + 1
+      }, 0),
+      takeWhile(acc => acc < attempt),
+      delay(timeDelay)
+    )
+  }
+}
+
+function renderMovie(movies: IMovie[]) {
+  movies.forEach((movie: IMovie) => {
+    let div = document.createElement('div');
+    div.innerText = movie.title;
+    output.appendChild(div);
+  });
+}
+
+click.pipe(
+  switchMap(() => load('../movies.json'))
+).subscribe({
+  next: renderMovie,
+  error: (e: Error) => console.log(`Erro: ${e}`),
+  complete: () => console.log('Complete'),
+})
